b4cfc5c3aefba04d58ced9edf5c4eb219f758f17
Pascal Knecht <pascal.knecht@hsr.ch>
2019-06-22 14:35:04 +0200

    improve parallelism code
    reducing the amount of threads and blocking operations to one

--- src/scanner/scanner.cpp
+++ src/scanner/scanner.cpp
@@ -2,16 +2,24 @@
 
 #include <algorithm>
 
-void nxscan::scanner::Scanner::scan() {
+std::vector<nxscan::scanner::Resolver> prepareScan(std::vector<nxscan::scanner::Probes> &allProbes,
+                                                   boost::asio::io_service &io_service) {
+  std::vector<nxscan::scanner::Resolver> allResolvers{};
   std::for_each(allProbes.begin(), allProbes.end(), [&](nxscan::scanner::Probes &p) {
-    boost::asio::io_service io_service;
-    nxscan::scanner::Resolver resolver1{io_service, p.getDomain()};
-    resolver1.resolve();
-    nxscan::scanner::Resolver resolver2{io_service, p.getHost()};
-    resolver2.resolve();
-    nxscan::scanner::Resolver resolver3{io_service, p.getRandomHost()};
-    resolver3.resolve();
-    io_service.run();
+    allResolvers.emplace_back(io_service, p.getDomain());
+    allResolvers.emplace_back(io_service, p.getHost());
+    allResolvers.emplace_back(io_service, p.getRandomHost());
+  });
+  return allResolvers;
+}
+
+void nxscan::scanner::Scanner::scan() {
+  boost::asio::io_service io_service;
+  auto allResolvers = prepareScan(allProbes, io_service);
+
+  std::for_each(std::begin(allResolvers), std::end(allResolvers), [](auto &r) {
+    r.resolve();
   });
 
+  io_service.run();
 }
